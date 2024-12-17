916a7db0a4a78e95dbe09adc70cd35796753bd7e
Wushengyouhan <840835067@qq.com>
2024-12-17 21:29:14 +0800

    246. Custom Hook: Managing State & Returning State Values

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,9 +1,9 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-import Places from './Places.jsx';
-import Error from './Error.jsx';
-import { sortPlacesByDistance } from '../loc.js';
-import { fetchAvailablePlaces } from '../http.js';
+import Places from "./Places.jsx";
+import Error from "./Error.jsx";
+import { sortPlacesByDistance } from "../loc.js";
+import { fetchAvailablePlaces } from "../http.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
@@ -16,20 +16,16 @@ export default function AvailablePlaces({ onSelectPlace }) {
 
       try {
         const places = await fetchAvailablePlaces();
-
-        navigator.geolocation.getCurrentPosition((position) => {
-          const sortedPlaces = sortPlacesByDistance(
-            places,
-            position.coords.latitude,
-            position.coords.longitude
-          );
+        // 模拟获取经纬度的延迟
+        setTimeout(() => {
+          const sortedPlaces = sortPlacesByDistance(places, 30, 150);
           setAvailablePlaces(sortedPlaces);
           setIsFetching(false);
-        });
+        }, 2000);
       } catch (error) {
         setError({
           message:
-            error.message || 'Could not fetch places, please try again later.',
+            error.message || "Could not fetch places, please try again later.",
         });
         setIsFetching(false);
       }
