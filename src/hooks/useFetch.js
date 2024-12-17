62e1de1fb799375c22c74d7994a49ff30a6466c3
Wushengyouhan <840835067@qq.com>
2024-12-17 22:38:57 +0800

    247. Exposing Nested Functions From The Custom Hook

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -21,5 +21,5 @@ export function useFetch(fetchFn, initialValue) {
     fetchData();
   }, [fetchFn]);
 
-  return { isFetching, fetchedData, error };
+  return { isFetching, fetchedData, setFetchedData, error };
 }
